9d9f367fbfc05c8c60ddd5bb393dc0fa2da91b7c
ErikP0 <erik.pohle@polytechnique.edu>
2021-02-16 15:21:49 +0100

    Added basic simulation

--- deep_cure_learning/envs/deep_cure_env.py
+++ deep_cure_learning/envs/deep_cure_env.py
@@ -1,33 +1,67 @@
-import gym 
+import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+
+class ForeignCountry:
+    """
+    Environment simulation of a country not under the control of the agent
+    """
+    def __init__(self, border_traffic, number_of_infected, population, save_history=False):
+        """
+        border_traffic: percentage of infected that cross over to the agent's country
+        number_of_infected: number of infected citizens of this foreign country
+        population: total number of citizens of this foreign country
+        save_history: If true, a history (for plotting) is saved
+        """
+        self.border_traffic = border_traffic
+        self.number_of_infected = number_of_infected
+        self.population = population
+        self.save_history = save_history
+        if self.save_history:
+            self.hist_infected = []
+
+    def step(self, infection_rate):
+        """
+        Simulate a time step. Computes the new number of infected in this foreign country
+        infection_rate: the R-factor of the virus in this country
+        """
+        self.number_of_infected = min(self.population, infection_rate * self.number_of_infected)
+        if self.save_history:
+            self.hist_infected.append(self.number_of_infected)
+
+class Measure:
+    """
+    A measure that (if active) reduces the R-factor by infection_rate_impact but
+    decreases the score by score_impact
+    """
+    def __init__(self, name, infection_rate_impact, score_impact):
+        """
+        name: The name of the measure
+        infection_rate_impact: subtracted from the R-factor if the measure is active
+        score_impact: subtracted from the score if the measure is active (i.e. a penalty)
+        """
+        self.name = name
+        self.infection_rate_impact = infection_rate_impact
+        self.score_impact = score_impact
+
+class Observation:
+    def __init__(self):
+        pass
 
 class DeepCure(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
-        self.intern_infect_rate = 0
-        self.hospital_count = 0
-        self.morale_score = 0
-        self.v_base_infect_rate = 0
-        self.v_severity = 0
-        self.v_lethality = 0
-        self.masks = 0
-        self.curfew = 0
-        self.borders_open = 0
-        self.impact_masks = 0
-        self.impact_curfew = 0
-        self.impact_borders_open = 0
-		self.counter = 0
-		self.reward = 0
-        self.f_countries = []
+    def __init__(self, save_history=False):
+        self.save_history = save_history
+        self.reset()
 
-    def _step(self, action):
+    def step(self, action):
         """
 
         Parameters
         ----------
-        action :
+        action : [mask: boolean, curfew: boolean, border_open: list of boolean]
+        Indicate the action to take
 
         Returns
         -------
@@ -51,42 +85,109 @@ class DeepCure(gym.Env):
                  However, official evaluations of your agent are not allowed to
                  use this for learning.
         """
-        self._take_action(action)
-        for i, (traffic, nb_infected) in enumerate(self.f_countries):
-            self.f_countries[i][1] = nb_infected * self.v_base_infect_rate
+        # apply action
+        self.take_action(action)
+        # simulate environment
+        for fcountry in self.f_countries:
+            fcountry.step(self.v_base_infect_rate)
 
-        self.intern_infect_rate = self.v_base_infect_rate - self.impact_borders_open
-        internal infection rate = base infection rate - measures
-        number of infected = internal infection rate * old number of infected + (border traffic * percentage infected people in the foreign country if border is not closed)
-        number hospitalizations = number infected * severity
-        number dead = (number hospitalizations - number available places) + remaining * lethality
+        # compute real R factor taking measures into account
+        self.intern_infect_rate = self.v_base_infect_rate - sum([m.infection_rate_impact for i,m in enumerate(self.available_measures) if self.measures_active[i]])
 
-    score =  measures + dead + hospitalizations
+        # compute infections caused by border traffic
+        infected_from_foreign_countries = sum([c.border_traffic * c.number_of_infected for i,c in enumerate(self.f_countries) if self.borders_open[i]])
 
-        reward = self._get_reward()
-        ob = self.env.getState()
-        episode_over = self.status != hfo_py.IN_GAME
-        return ob, reward, episode_over, {}
+        # compute system state for new time step
+        self.number_of_infected = min(self.population, self.intern_infect_rate * self.number_of_infected + infected_from_foreign_countries)
+        self.number_of_dead = self.v_lethality * max(0, self.number_of_severe - self.hospital_capacity)
+        self.number_of_severe = self.v_severity * self.number_of_infected
 
-    def _reset(self):
+        self.reward = self.get_reward()
+
+        if self.save_history:
+            self.hist_internal_infection_rate.append(self.intern_infect_rate)
+            self.hist_infected.append(self.number_of_infected)
+            self.hist_severe.append(self.number_of_severe)
+            self.hist_dead.append(self.number_of_dead)
+            self.hist_reward.append(self.reward)
+            self.hist_border.append(infected_from_foreign_countries)
+
+        self.t += 1
+
+        ob = Observation()
+        return ob, self.reward, self.is_episode_over(), {}
+
+    def reset(self):
+        # the total number of citizens
+        self.population = 1000
+        # the number of infected citizens at the current time step
+        self.number_of_infected = 1
+        # the number of dead citizens at the current time step
+        self.number_of_dead = 0
+        # the number of severe cases of the illness
+        self.number_of_severe = 0
+        # the capacity of hospitals. Severe cases that aren't treated in the hospital may lead to death
+        self.hospital_capacity = 100
+        # the true R-factor in the agent's country
+        self.intern_infect_rate = 0
+
+        # Environment:
+        # List of foreign countries
+        self.f_countries = [ForeignCountry(0.01,10,1000, self.save_history)]
+
+        # the virus's base R-factor
+        self.v_base_infect_rate = 1.5
+        # the percentage of severe cases in case of infection
+        # E.g. if v_severity=0.5, then 50% of all infections are severe cases
+        self.v_severity = 0.5
+        # the percentage of lethal outcome in case of severe case without hospitalization
+        # E.g. if v_severity=0.5, then 50% of non-hospitalized severe cases lead to death
+        self.v_lethality = 0.5
+
+        # current time step
+        self.t = 0
+        # the current score
+        self.reward = 0
+        # score weighting for the number of dead citizens
+        self.weight_dead = 1
+        # score weighting for the number of severe cases
+        self.weight_severe = 0.5
+
+        # Measures:
+        # If borders_open[i] is true, then border traffic from foreign country i is allowed
+        self.borders_open = [True] * len(self.f_countries)
+        # A list of available measures
+        self.available_measures = [Measure('Masks', 0.1, 0.1), Measure('Curfew', 0.5, 0.5)]
+        # If measures_active[i] is true, then measure available_measures[i] is active
+        self.measures_active = [False] * len(self.available_measures)
+
+        if self.save_history:
+            self.hist_internal_infection_rate = []
+            self.hist_infected = []
+            self.hist_severe = []
+            self.hist_dead = []
+            self.hist_reward = []
+            self.hist_border = []
+
+    def render(self, mode='human', close=False):
         pass
 
-    def _render(self, mode='human', close=False):
-        pass
-
-    def _take_action(self, action):
+    def take_action(self, action):
         """
         action: [ masks, curfew, borders]
         """
-        self.masks = action[0]
-        self.curfew = action[1]
+        self.measures_active[0] = action[0]
+        self.measures_active[1] = action[1]
+        assert(len(action[2]) == len(self.f_countries))
         self.borders_open = action[2]
 
-    def _get_reward(self):
-        """ Reward is given for XY. """
-        if self.status == FOOBAR:
-            return 1
-        elif self.status == ABC:
-            return self.somestate ** 2
-        else:
-            return 0
+    def get_reward(self):
+        """ Penality for active measures, number of severe cases and number of dead """
+        return -sum(m.score_impact for i,m in enumerate(self.available_measures) if self.measures_active[i]) - self.weight_severe * self.number_of_severe - self.weight_dead * self.number_of_dead
+
+    def is_episode_over(self):
+        """
+        The episode is over if
+        - the whole population dies
+        """
+        return self.number_of_dead >= self.population
